9a088d89b94b85f4484ee420f4d08158fd3ffacb
Keith Billings <keith.billings@brandlive.com>
2022-05-08 10:32:10 -0700

    update blurbs in cards

--- components/NavCards.js
+++ components/NavCards.js
@@ -7,8 +7,8 @@ import sampleImage3 from "../public/sample/sample-image-3.jpg";
 export default function NavCards() {
 	const webDevImages = [sampleImage1, sampleImage2, sampleImage3];
 
-	const webDevContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit, esse ad dolorem odit autem.";
-	const photographyContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit, esse ad dolorem odit autem.";
+	const webDevContent = "Employed at award winning company, Brandlive. Front-end specialist with a knack for problem solving.";
+	const photographyContent = "14 years of experience, began freelance in 2020. Specialize in portraits.";
 	const aboutMeContent = "Born and raised in Portland, Oregon. Read the journey of Keith Billings. From firefighting, to building maintenance, and now thriving in tech.";
 
 	return (
@@ -16,7 +16,7 @@ export default function NavCards() {
 			<h2 className="nav-cards__title">Categories</h2>
 			<NavCard title={"Web Development"} images={webDevImages} content={webDevContent} path={"/portfolio/webdevelopment"} buttonText={`Say "Hello" to my "World"`} />
 			<NavCard title={"Photography"} images={webDevImages} content={photographyContent} path={"/portfolio/photography"} buttonText={"Peer through the lens"} />
-			<NavCard title={"About Me"} images={webDevImages} content={aboutMeContent} path={"/about"} buttonText={"Learn more"} />
+			<NavCard title={"About"} images={webDevImages} content={aboutMeContent} path={"/about"} buttonText={"Learn more"} />
 		</div>
 	);
 }
